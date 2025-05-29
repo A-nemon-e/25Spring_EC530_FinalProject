b9b138e83f0e28b1a667b2e9a25c6543cbd577da
A-nemon-e <a-nemon-e@outlook.com>
2025-05-29 16:25:13 +0800

    侧栏保持文件详情历史，修复侧栏不高亮

--- frontend/frontend-ui/src/components/Sidebar.vue
+++ frontend/frontend-ui/src/components/Sidebar.vue
@@ -1,10 +1,14 @@
 <!-- src/components/Sidebar.vue -->
 <template>
-  <el-menu :default-active="activeMenu" router class="el-menu-vertical-demo">
+  <el-menu
+  :key="activeMenu"                
+  :default-active="activeMenu"
+  router
+  class="el-menu-vertical-demo"
+>
     <el-menu-item index="/search">🔍 搜索</el-menu-item>
     <el-menu-item :index="folderViewRoute">📂 文件夹视图</el-menu-item>
-
-    <el-menu-item index="/file">📄 文件详情视图</el-menu-item>
+    <el-menu-item :index="fileDetailRoute">📄 文件详情视图</el-menu-item>
     <el-divider />
     <el-menu-item index="/tag-manager">🏷️ 标签管理</el-menu-item>
     <el-menu-item index="/alias-manager">🔁 别名设置</el-menu-item>
@@ -22,14 +26,23 @@ const store = useFolderExplorerStore()
 const folderViewRoute = computed(() => {
   return store.folderId ? `/folder/${store.folderId}` : '/folder'
 })
+import { useFileStore } from '../stores/fileStore'
+
+const fileStore = useFileStore()
+
+const fileDetailRoute = computed(() => {
+  // console.log(route.path)
+  return fileStore.fileData?.id ? `/file/${fileStore.fileData.id}` : '/file'
+})
+
 
 
 const route = useRoute()
 
 const activeMenu = computed(() => {
   const path = route.path
-  if (path.startsWith('/folder')) return '/folder'
-  if (path.startsWith('/file')) return '/file'
+  if (path.startsWith('/folder')) return folderViewRoute.value
+  if (path.startsWith('/file')) return fileDetailRoute.value
   if (path.startsWith('/search')) return '/search'
   if (path.startsWith('/tag-manager')) return '/tag-manager'
   if (path.startsWith('/alias-manager')) return '/alias-manager'
