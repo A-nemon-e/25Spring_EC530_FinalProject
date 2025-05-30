8ea18fd4882a8531a400bd9065b5584d695241ec
A-nemon-e <a-nemon-e@outlook.com>
2025-05-30 22:49:09 +0800

    代理5000/api

--- frontend/frontend-ui/vite.config.js
+++ frontend/frontend-ui/vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    },
+    host: '0.0.0.0',  // 允许外部访问
+    port: 5173
+  }
+}
