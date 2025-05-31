ca20e0113b1db66620623e01018a9c6378d58f5c
A-nemon-e <a-nemon-e@outlook.com>
2025-05-31 10:29:43 +0800

    Vite挂载中间件可以输出请求

--- frontend/frontend-ui/vite.config.js
+++ frontend/frontend-ui/vite.config.js
@@ -2,14 +2,29 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'custom-logger-middleware',
+      configureServer(server) {
+        console.log('[vite] 中间件已挂载')
+
+        server.middlewares.use((req, res, next) => {
+          const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+          const time = new Date().toISOString()
+          console.log(`[${time}] ${ip} ${req.method} ${req.url}`)
+          next()
+        })
+      }
+    }
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
+        changeOrigin: true
       }
     }
   }
